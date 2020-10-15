050e31541c0a2f87e33dcbf2b42999675bd01fca
Jonathan Cabrera <qfeaso@gmail.com>
2020-10-14 22:19:20 -0700

    file inclusion process

---
+++ Pods/Target Support Files/Pods-PGram-PGramUITests/Pods-PGram-PGramUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PGram_PGramUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PGram_PGramUITests
+@end
---
+++ Pods/Target Support Files/Pods-PGramTests/Pods-PGramTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PGramTests : NSObject
+@end
+@implementation PodsDummy_Pods_PGramTests
+@end
